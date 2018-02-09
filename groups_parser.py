23755cb38e8a01af99ba671f18144f51e3a8963a
Ilya Osipov <070809010@mail.ru>
2018-02-09 19:00:14 +0300

    Get dllCourse select having faculty id

--- groups_parser.py
+++ groups_parser.py
@@ -17,36 +17,27 @@ cookies = {
     '_ga': '0',
     'ASP.NET_SessionId': '0'
 }
-groupName = '291Д-08ИБ/16'
-week = 8
-day = '19.02.2018'
-lesson = 3
-get = {
-    'GroupName': groupName,
-    'Week': week
-}
-url = 'https://rasp.rea.ru/default?' + urlencode(get, quote_plus)
+
+url = 'https://rasp.rea.ru/default?' + urlencode('', quote_plus)
 session = requests.Session()
 response = session.get(url, headers=headers, cookies=cookies)
 dom = BeautifulSoup(response.content, 'html.parser')
-eventTarget = 'upModal'
-eventArgument = str(groupName) + ':' + str(day) + ':' + str(lesson)
 lastFocus = dom.find('input', {'name': '__LASTFOCUS'}).get('value')
 viewState = dom.find('input', {'name': '__VIEWSTATE'}).get('value')
 viewStateGenerator = dom.find('input', {'name': '__VIEWSTATEGENERATOR'}).get('value')
 eventValidation = dom.find('input', {'name': '__EVENTVALIDATION'}).get('value')
 
 data = {'__ASYNCPOST': 'true',
-        '__EVENTARGUMENT': eventArgument,
-        '__EVENTTARGET': eventTarget,
+        '__EVENTARGUMENT': '',
+        '__EVENTTARGET': 'ddlFaculty',
         '__EVENTVALIDATION': eventValidation,
         '__LASTFOCUS': lastFocus,
         '__VIEWSTATE': viewState,
         '__VIEWSTATEGENERATOR': viewStateGenerator,
-        'ctl11': 'upModal|upModal',
+        'ctl11': 'upSelectGroup|ddlFaculty',
         'ddlBachelor': 'na',
         'ddlCourse': 'na',
-        'ddlFaculty': 'na',
+        'ddlFaculty': '4737',
         'ddlGroup': 'na',
         'txtGroupName': ''}
 
