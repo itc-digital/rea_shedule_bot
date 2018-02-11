c9d123f7624966f75ce7d45ac1634206f5201eac
wanku-x <wanku@bk.ru>
2018-02-11 16:56:52 +0300

    Add function question_faculty

--- groups_parser.py
+++ groups_parser.py
@@ -112,16 +112,21 @@ def post_main_page(data, target):
     dom = BeautifulSoup(response.content, 'html.parser')
     return dom
 
-dom = get_main_page()   # Получаем главную страницу
-faculty_dict = parse_select(dom, 'ddlFaculty')  # Собираем факультеты
-print('Введи номер твоего факультета:') # Справшиваем номер факультета
-for index, faculty in enumerate(faculty_question):
-    print(index+1, ':', faculty)
+# Функция спрашивания факультета
+def question_faculty():
+    print('Введи номер твоего факультета:')
+    for index, faculty in enumerate(faculty_question):
+        print(index+1, ':', faculty)
 
-faculty_answer = int(input())-1     # Получаем ответ и проверяем
-if (faculty_answer<0) or (faculty_answer>10):
-    print('Пошёл на хуй')
-    exit()
+    faculty_answer = int(input())-1     # Получаем ответ и проверяем
+    if (faculty_answer<0) or (faculty_answer>10):
+        return False
+    else:
+        return faculty_answer
+
+dom = get_main_page()
+faculty_dict = parse_select(dom, 'ddlFaculty')
+faculty_answer = question_faculty()
 
 data = {
     '__EVENTVALIDATION': event_validation(dom),
@@ -135,52 +140,3 @@ data = {
 }
 response_course = post_main_page(data, 'ddlFaculty')
 print(response_course)
-
-
-'''
-import requests
-from urllib.parse import urlencode, quote_plus
-from bs4 import BeautifulSoup
-
-headers = {
-    'Host': 'rasp.rea.ru',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-    'Accept-Encoding': 'gzip, deflate',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-    'X-MicrosoftAjax': 'Delta=true',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Cache-Control': 'no-cache',
-    'Referer': 'https://rasp.rea.ru/'
-}
-cookies = {
-    '_ym_uid': '0',
-    '_ga': '0',
-    'ASP.NET_SessionId': '0'
-}
-
-url = 'https://rasp.rea.ru/default?' + urlencode('', quote_plus)
-session = requests.Session()
-response = session.get(url, headers=headers, cookies=cookies)
-dom = BeautifulSoup(response.content, 'html.parser')
-lastFocus = dom.find('input', {'name': '__LASTFOCUS'}).get('value')
-viewState = dom.find('input', {'name': '__VIEWSTATE'}).get('value')
-viewStateGenerator = dom.find('input', {'name': '__VIEWSTATEGENERATOR'}).get('value')
-eventValidation = dom.find('input', {'name': '__EVENTVALIDATION'}).get('value')
-
-data = {'__ASYNCPOST': 'true',
-        '__EVENTARGUMENT': '',
-        '__EVENTTARGET': 'ddlFaculty',
-        '__EVENTVALIDATION': eventValidation,
-        '__LASTFOCUS': lastFocus,
-        '__VIEWSTATE': viewState,
-        '__VIEWSTATEGENERATOR': viewStateGenerator,
-        'ctl11': 'upSelectGroup|ddlFaculty',
-        'ddlBachelor': 'na',
-        'ddlCourse': 'na',
-        'ddlFaculty': '4737',
-        'ddlGroup': 'na',
-        'txtGroupName': ''}
-
-response = session.post(url, data=data, headers=headers)
-print(response.text)
-'''
