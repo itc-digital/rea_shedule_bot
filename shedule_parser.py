250fcb52d416d8f58df08faa687cd4c77ac21f4b
Ilya Osipov <070809010@mail.ru>
2018-02-18 18:52:52 +0300

    Parse not only class title

--- shedule_parser.py
+++ shedule_parser.py
@@ -4,16 +4,22 @@ from bs4 import BeautifulSoup
 REA_HREF = 'https://rasp.rea.ru/default'
 
 
-def parse_day(rows):
+def serialize_day(rows):
     day_title = rows[0].findAll("td")[1].font.b.contents[0]
     serialized_day = {}
-    for day in rows[1:]:
-        day_data = day.findAll("td")
-        class_number = day_data[0].span.contents[0]
-        class_data = None
-        if day_data[1].a:
-            class_data = day_data[1].a.span.contents[0]
-        serialized_day[class_number] = class_data
+    for class_ in rows[1:]:
+        class_data = class_.findAll("td")
+        class_number = class_data[0].span.contents[0]
+        if not class_data[1].a:
+            continue
+        class_title = class_data[1].a.span.contents[0]
+        class_type = class_data[1].a.span.contents[2][1:]
+        class_room = class_data[1].a.span.contents[4]
+        serialized_class = {}
+        serialized_class['title'] = class_title
+        serialized_class['type'] = class_type
+        serialized_class['room'] = class_room
+        serialized_day[class_number] = serialized_class
     return day_title, serialized_day
 
 
@@ -25,6 +31,6 @@ def parse_shedule(group, week):
     serialized_week = {}
     for day_number in range(6):
         current_day = shedule_table[day_number * 9:day_number * 9 + 9]
-        day_title, serialized_day = parse_day(current_day)
+        day_title, serialized_day = serialize_day(current_day)
         serialized_week[day_title] = serialized_day
     return serialized_week
