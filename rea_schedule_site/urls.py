93564e530f93c64b042fc8326acfb690a63bb8aa
Ilya Osipov <070809010@mail.ru>
2018-02-22 11:48:04 +0300

    Fix urls file

--- rea_schedule_site/urls.py
+++ rea_schedule_site/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import include, url
+from django.urls import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('django_telegrambot.urls')),
+    path('', include('django_telegrambot.urls')),
 ]
