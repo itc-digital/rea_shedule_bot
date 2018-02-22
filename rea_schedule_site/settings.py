626e8abadfa9f4fbaeb01da35f5e2256a198cc23
Ilya Osipov <070809010@mail.ru>
2018-02-22 11:45:13 +0300

    Update ALLOWED_HOSTS

--- rea_schedule_site/settings.py
+++ rea_schedule_site/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 TELEGRAM_APPNAME = os.environ['APPNAME']
 
-ALLOWED_HOSTS = ['reascheduletest.herokuapp.com']
+ALLOWED_HOSTS = ['reascheduletest.herokuapp.com', '127.0.0.1']
 
 APPEND_SLASH = False
 
